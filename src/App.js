a1b2510db822ce32acf25e72976e9d0d325bfed5
David Gillberg <mollimers@gmail.com>
2021-01-06 11:35:16 +0100

    Add header and styling

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
         <div className="App">
+            <h1> Monsters Rollodex</h1>
             <SearchBox
                 placeholder='search monsters'
                 handleChange={e =>
